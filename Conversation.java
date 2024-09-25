ea4986e23d7470d58cc4d3577e80de40feb2e030
Lily Ruddy <lruddy@smith.edu>
2024-09-24 20:39:33 -0400

    starting on canned responses

--- Conversation.java
+++ Conversation.java
@@ -1,46 +1,61 @@
-import java.util.List;
 import java.util.Scanner;
+import java.util.Random;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
+
+// Used: https://www.freecodecamp.org/news/how-to-initialize-a-java-list/
 
 
 public class Conversation {
 
   // Attributes:
-  /* Store the number of rounds */
-  Scanner input;
-  String[] possible_questions;
+  /* Takes the response from user */
+  Scanner input = new Scanner(System.in); // scanner for round number
+  Scanner input2 = new Scanner(System.in); // scanner for user input
+  /* Random numbers */
+  Random rand = new Random();
+  /* List of arrays to store transcript */
+  ArrayList<String> transcript = new ArrayList<>();
+
   
-
-
-
-  // Constructor:
-  public Conversation(){
-    this.input = new Scanner(System.in);
-
-
-
-
-  }
   
+  // User imput and their mirror words:
+  String[] user_words = {"I", "me", "am", "you", "my", "your"};
+  String[] mirror = {"you", "me", "are", "I", "your", "my"};
+  String[] responses = {"Mmmmmm", "Wow", "Love that!", "Great!", "Tell me more"};
 
-
-  // Loop:
+  // Main loop:
   public void play(){
     System.out.println("How many rounds would you like to converse?");
-    int rounds = this.input.nextInt(); 
+    int rounds = input.nextInt(); 
 
+    System.out.println("Hi friend! What's on your mind?");
+    
     // for loop of the main conversation
-    for (int i=0; i<rounds; i++){
-      System.out.println("Hello friend! How was your day today?");
-      String feeling = this.input.next();
-      System.out.println("You're feeling " + feeling +"?");
+    for (int i = 0; i < rounds; i++){
+      String userInput = input2.nextLine(); // stores the user input
+      System.out.println("Lovely");
+      
 
 
+
+      /* User input */
+      // String userInput = input.nextLine();
+
+      // System.out.println(userInput);
+      // String[] eachUser = userInput.split(" ");
+      // System.out.println(eachUser[1]);
+
+
+
+    
+      
+      //for loop of checking each of the words in the user's response and checking if they match the mirror words
+      // for(int j=0; j<userInput.length()){
 
     }
-
-
-
-
 
 
   }
@@ -58,16 +73,21 @@ public class Conversation {
 
 
 
-// Idea Dump
-    // ask name
-    // if(rounds > 1){
-    //   System.out.println("Hello friend! What would you like me to call you?");
-    //   String name = this.input.next();
-    //   System.out.println("It is nice to meet you"+ name);
-    // } else{
-    //   System.out.println("Hi how are you!");
-    // }
 
-    // Use
-    //if word.equal(){
-    //}
+// Idea Dump
+  // int size = p_questions_list.length;
+  // p_questions[rand.nextInt(p_questions.length)];
+
+  // random integer
+    // int rand_int1 = rand.nextInt(rounds);
+    //System.out.println(rand.nextInt(rounds));
+
+
+  // Possible Questions:
+    /* Creating an array with potential questions */
+    // String[] p_questions = {"How was your day?", 
+    //   "What is your favorite color", 
+    //   "What do you like to do for fun?"};
+  
+  /* Creating a list object */
+    // List<String> p_questions_list = Arrays.asList(p_questions);
