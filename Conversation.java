bcdfaf4153d73f135c1d6842f3e8493cb9e657d9
Lily Ruddy <lruddy@smith.edu>
2024-09-25 15:47:50 -0400

    finishing the rubic and reflection

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.util.Random;
-import java.util.Arrays;
+//import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -9,7 +9,10 @@ import java.util.ArrayList;
 //https://www.freecodecamp.org/news/how-to-initialize-a-java-list/
 //https://stackoverflow.com/questions/858572/how-to-make-a-new-list-in-java
 
-
+/**
+ * Begins a conversation with a chatbox. 
+ * 
+ */
 public class Conversation {
 
   // Attributes:
@@ -20,27 +23,28 @@ public class Conversation {
   Random rand = new Random();
   /* List of arrays to store transcript */
   ArrayList<String> transcript = new ArrayList<>();
-
-  
   
   // User Input & Mirror Words:
-  String[] user_words = {"I", "me", "am", "you", "my", "your"};
+  String[] userWords = {"I", "me", "am", "you", "my", "your"};
   String[] mirror = {"you", "me", "are", "I", "your", "my"};
   String[] responses = {"Mmmmmm.", "Wow!", "Love that!", "Great!", "Please, tell me more.", "Interesting."};
 
+  /**
+   * Play function that starts the main conversation with the chatbox.
+   * @return Returns print statements detailing the conversation. 
+   */
   // Main Loop:
   public void play(){
     System.out.println("How many rounds would you like to converse?");
     int rounds = input.nextInt(); 
-
-    System.out.println("Hi friend! What's on your mind?");
+    System.out.println("Hi friend! What's on your mind?"); //prompt question
     
     // For Loop of the Main Conversation
     for (int i = 0; i < rounds; i++){
       String userInput = input2.nextLine(); // stores the user input  
       /* Storing the indexes of the user and mirror words */
-      List<Integer> user_match = new ArrayList<>(); // stores indexes for user
-      List<Integer> mirror_match = new ArrayList<>(); // stores indexes for mirror
+      List<Integer> userMatch = new ArrayList<>(); // stores indexes for user
+      List<Integer> mirrorMatch = new ArrayList<>(); // stores indexes for mirror
       
       /* Detects mirror words in user input*/
       String[] eachUser = userInput.split(" "); // splits user input by their spaces
@@ -48,16 +52,14 @@ public class Conversation {
 
  
         /*for loop of checking each of the words in the user's response and checking if they match the mirror words */
-        for(int k = 0; k< user_words.length; k++){ // for each word in user_words compare
+        for(int k = 0; k< userWords.length; k++){ // for each word in user_words compare
           
           
-          if (eachUser[j].equalsIgnoreCase(user_words[k])) {
-            System.out.println(eachUser[j] + " matches " +user_words[k]);
-            user_match.add(j); // adds the index
-            mirror_match.add(k); // adds the index
+          if (eachUser[j].equalsIgnoreCase(userWords[k])) {
+            //System.out.println(eachUser[j] + " matches " +user_words[k]);
+            userMatch.add(j); // adds the index
+            mirrorMatch.add(k); // adds the index
             
-            
-
             
           } else{
 
@@ -65,39 +67,22 @@ public class Conversation {
           }
           
           
-          
 
         }
         
-        //eachUser[j].equalsIgnoreCase()
-        
-        //System.out.println(eachUser[j]);
+      
 
 
       }
-
-
-
-
-      //System.out.println("user_match is " + user_match + "and mirror_match is" + mirror_match); // prints out 
       
       // Randomizes Canned Responses
       int rand_int1 = rand.nextInt(responses.length); // randomly chooses a number from 0 to the number of responses in responses
       System.out.println(responses[rand_int1]); // replies with canned responses
 
-      //System.out.println(eachUser[j] + " doesn't match " +user_words[k]);
-      
-    
-      
-      
-
     }
     // Outside of For loop
     /* Thanking the user for talking with them */
     System.out.println("Thank you for talking with me. I enjoyed our conversation. Please come again to chat!");
-
-
-
 
   }
 
@@ -110,9 +95,3 @@ public class Conversation {
     myConversation.play();
   }
 }
-
-
-
-
-
-// Idea Dump:
--- reflection.md
+++ reflection.md
@@ -6,4 +6,5 @@ Use this file to record your reflection on this assignment.
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
 
-- talked with my friend Lilliana and went to TA hours Lan & Zihan & Ashley
+
+I talked with my friend Lilliana and went to TA hours where I talked with Lan, Zihan, and Ashley. I went on Tuesday from 7pm - 9pm. I was really struggling with the assignment, because I kept getting tripped up with the way Java is formatted. A lot of things are similar with Python, but there are enough differences that coding in Java gave me challenges. For loops and if/else statements work the same, but I had trouble formatting them. Scanners and saving the user's response to an object was difficult. I also had trouble differentiating when to use lits or when to use arrays. I kept getting errors when I tried making them. Overall, the formatting and converting things found in Python to Java gave me difficulties. For advice, I would suggest starting this assignment earlier. I thought I gave myself enough time to finish this homework, but I needed more help than I anticipated.
